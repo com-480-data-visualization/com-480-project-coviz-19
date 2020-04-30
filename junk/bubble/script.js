74936752b958f817d09c2444eef2f8ac62cec695
thirteenfoil8 <thirteenfoil8@users.noreply.github.com>
2020-04-30 14:23:10 +0200

    try another graph

--- junk/bubble/script.js
+++ junk/bubble/script.js
@@ -1,94 +1,83 @@
-
-
-dataset = {
-    "children": [{"Name":"PSG","Count":12},
-        {"Name":"Lille","Count":11},
-        {"Name":"Montpellier","Count":10},
-        {"Name":"Rennes","Count":9},
-        {"Name":"Marseille","Count":8},
-        {"Name":"Nice","Count":8},
-        {"Name":"Angers","Count":8},
-        {"Name":"Monaco","Count":8},
-        {"Name":"Strasbourg","Count":7},
-        {"Name":"Brest","Count":6},
-        {"Name":"Dijon","Count":6},
-        {"Name":"Nantes","Count":6},
-        {"Name":"Nimes","Count":6},
-        {"Name":"Lyon","Count":5},
-        {"Name":"Metz","Count":5},
-        {"Name":"Reims","Count":5},
-        {"Name":"St Etienne","Count":4},
-        {"Name":"Bordeaux","Count":4},
-        {"Name":"Amiens","Count":3},
-        {"Name":"Toulouse","Count":3}]
-
-};
-
+var margin = {top: 10, right: 30, bottom: 30, left: 60};
 
 var diameter = 600;
-var color = d3.scaleOrdinal(d3.schemeCategory20);
-
-var bubble = d3.pack(dataset)
-    .size([diameter, diameter])
-    .padding(1.5);
 
 var svg = d3.select("body")
     .append("svg")
-    .attr("width", diameter)
-    .attr("height", diameter)
-    .attr("class", "bubble");
-
-var nodes = d3.hierarchy(dataset)
-    .sum(function(d) { return d.Count; });
-
-var node = svg.selectAll(".node")
-    .data(bubble(nodes).descendants())
-    .enter()
-    .filter(function(d){
-        return  !d.children
-    })
+      .attr("width", diameter + margin.left + margin.right)
+      .attr("height", diameter + margin.top + margin.bottom)
+      .attr("class", "bubble")
     .append("g")
-    .attr("class", "node")
-    .attr("transform", function(d) {
-        return "translate(" + d.x + "," + d.y + ")";
-    });
+      .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")");
 
-node.append("title")
-    .text(function(d) {
-        return d.Name + ": " + d.Count;
-    });
+d3.csv("https://raw.githubusercontent.com/com-480-data-visualization/com-480-project-coviz-19/master/junk/bubble/F1.csv",
 
-node.append("circle")
-    .attr("r", function(d) {
-        return d.r;
-    })
-    .style("fill", function(d,i) {
-        return color(i);
-    });
+  function (d){
+    return { Div : d.Div, HomeTeam : d.HomeTeam, Win : d.Win }
+  },
 
-node.append("text")
-    .attr("dy", ".2em")
-    .style("text-anchor", "middle")
-    .text(function(d) {
-        return d.data.Name.substring(0, d.r / 3);
-    })
-    .attr("font-family", "sans-serif")
-    .attr("font-size", function(d){
-        return d.r/5;
-    })
-    .attr("fill", "white");
+  function (data) {
+    console.log(data)
 
-node.append("text")
-    .attr("dy", "1.3em")
-    .style("text-anchor", "middle")
-    .text(function(d) {
-        return d.data.Count;
-    })
-    .attr("font-family",  "Gill Sans", "Gill Sans MT")
-    .attr("font-size", function(d){
-        return d.r/5;
-    })
-    .attr("fill", "white");
+    var color = d3.scaleOrdinal(d3.schemeCategory20);
 
-d3.select(self.frameElement)
-    .style("height", diameter + "px");
+    var bubble = d3.pack(data)
+        .size([diameter, diameter])
+        .padding(1.5);
+
+
+    var nodes = d3.hierarchy(data)
+        .sum(function(d) { return d.Win; });
+
+    var node = svg.selectAll(".node")
+        .data(bubble(nodes).descendants())
+        .enter()
+        .append("g")
+        .attr("class", "node")
+        .attr("transform", function(d) {
+            return "translate(" + d.x + "," + d.y + ")";
+        });
+
+    node.append("title")
+        .text(function(d) {
+
+            return d.HomeTeam + ": " + d.Win;
+        });
+
+    node.append("circle")
+        .attr("r", function(d) {
+            return d.r;
+        })
+        .style("fill", function(d,i) {
+            return color(i);
+        });
+    node.append("text")
+        .attr("dy", ".2em")
+        .style("text-anchor", "middle")
+        .text(function(d) {
+            console.log(d.data[0].HomeTeam)
+            return d.data[0].HomeTeam.substring(0, d.r / 3);
+        })
+        .attr("font-family", "sans-serif")
+        .attr("font-size", function(d){
+            return d.r/5;
+        })
+        .attr("fill", "white");
+
+    node.append("text")
+        .attr("dy", "1.3em")
+        .style("text-anchor", "middle")
+        .text(function(d) {
+            console.log(d.data[0].Win)
+            return d.data[0].Win;
+        })
+        .attr("font-family",  "Gill Sans", "Gill Sans MT")
+        .attr("font-size", function(d){
+            return d.r/5;
+        })
+        .attr("fill", "white");
+
+    d3.select(self.frameElement)
+        .style("height", diameter + "px");
+  })
