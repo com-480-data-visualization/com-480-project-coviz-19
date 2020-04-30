c6864c1e97b462e25e8ae1487209275f40a093e0
thirteenfoil8 <thirteenfoil8@users.noreply.github.com>
2020-04-30 15:47:26 +0200

    make the bubble better

--- skeleton/bubble/script.js
+++ skeleton/bubble/script.js
@@ -1,83 +1,150 @@
-var margin = {top: 10, right: 30, bottom: 30, left: 60};
 
-var diameter = 600;
+// set the dimensions and margins of the graph
+var width = 600
+var height = 600
+// append the svg object to the body of the page
+var svg = d3.select("#my_dataviz")
+  .append("svg")
+    .attr("width", width)
+    .attr("height", height)
 
-var svg = d3.select("body")
-    .append("svg")
-      .attr("width", diameter + margin.left + margin.right)
-      .attr("height", diameter + margin.top + margin.bottom)
-      .attr("class", "bubble")
-    .append("g")
-      .attr("transform",
-            "translate(" + margin.left + "," + margin.top + ")");
+// Read data
+d3.csv("https://raw.githubusercontent.com/com-480-data-visualization/com-480-project-coviz-19/master/skeleton/bubble/test.csv", function(data) {
 
-d3.csv("https://raw.githubusercontent.com/com-480-data-visualization/com-480-project-coviz-19/master/junk/bubble/F1.csv",
+  // Filter a bit the data -> more than 1 game win
+  data = data.filter(function(d){ return d.value>1 })
 
-  function (d){
-    return { Div : d.Div, HomeTeam : d.HomeTeam, Win : d.Win }
-  },
+  // Color palette
+  var color = d3.scaleOrdinal()
+    .domain(["F1", "I1", "E0", "D1"])
+    .range(d3.schemeSet1);
 
-  function (data) {
-    console.log(data)
+  // Size scale for countries
+  var z = d3.scaleLinear()
+    .domain([0, 75])
+    .range([7,200])  // circle will be between 7 and 200 px wide
 
-    var color = d3.scaleOrdinal(d3.schemeCategory20);
+  // create a tooltip
+  var Tooltip = d3.select("#my_dataviz")
+    .append("div")
+    .style("opacity", 0)
+    .attr("class", "tooltip")
+    .style("background-color", "white")
+    .style("border", "solid")
+    .style("border-width", "2px")
+    .style("border-radius", "5px")
+    .style("padding", "5px")
 
-    var bubble = d3.pack(data)
-        .size([diameter, diameter])
-        .padding(1.5);
+  // Three function that change the tooltip when user hover / move / leave a cell
+  var mouseover = function(d) {
+    Tooltip
+      .transition()
+      .style("opacity", 1)
+  }
+  var mousemove = function(d) {
+    Tooltip
+      .html('<u>' + d.div +' '+ '</u> '+'<u>' + d.key + '</u>' + "<br>" + d.value + " wins")
+      .style("left", (d3.mouse(this)[0]+70) + "px")
+      .style("top", (d3.mouse(this)[1]) + "px")
+  }
+  var mouseleave = function(d) {
+    Tooltip
+      .style("opacity", 0)
+  }
+
+  var highlight = function(d){
+    // reduce opacity of all groups
+    d3.selectAll(".node").style("opacity", .05)
+    // expect the one that is hovered
+    d3.selectAll("."+d).style("opacity", 1)
+  }
+
+  // And when it is not hovered anymore
+  var noHighlight = function(d){
+    d3.selectAll(".node").style("opacity", 1)
+  }
+
+  // Initialize the circle: all located at the center of the svg area
+  var node = svg.append("g")
+    .selectAll("circle")
+    .data(data)
+    .enter()
+    .append("circle")
+      .attr("class", function(d) { return "node " + d.div })
+      .attr("r", function(d){ return z(d.value)})
+      .attr("cx", width / 2)
+      .attr("cy", height / 2)
+      .style("fill", function(d){ return color(d.div)})
+      .style("fill-opacity", 0.8)
+      .attr("stroke", "black")
+      .style("stroke-width", 1)
+      .on("mouseover", mouseover) // What to do when hovered
+      .on("mousemove", mousemove)
+      .on("mouseleave", mouseleave)
+      .call(d3.drag() // call specific function when circle is dragged
+           .on("start", dragstarted)
+           .on("drag", dragged)
+           .on("end", dragended));
 
 
-    var nodes = d3.hierarchy(data)
-        .sum(function(d) { return d.Win; });
 
-    var node = svg.selectAll(".node")
-        .data(bubble(nodes).descendants())
-        .enter()
-        .append("g")
-        .attr("class", "node")
-        .attr("transform", function(d) {
-            return "translate(" + d.x + "," + d.y + ")";
-        });
+     // Add one dot in the legend for each name.
+   var size = 20
+   var allgroups = ["F1", "I1", "E0", "D1"]
+   svg.selectAll("myrect")
+     .data(allgroups)
+     .enter()
+     .append("circle")
+       .attr("cx", 550)
+       .attr("cy", function(d,i){ return 10 + i*(size+5)}) // 100 is where the first dot appears. 25 is the distance between dots
+       .attr("r", 7)
+       .style("fill", function(d){ return color(d)})
+       .on("mouseover", highlight)
+       .on("mouseleave", noHighlight)
 
-    node.append("title")
-        .text(function(d) {
+     // Add labels beside legend dots
+   svg.selectAll("mylabels")
+     .data(allgroups)
+     .enter()
+     .append("text")
+       .attr("x", 550 + size*.8)
+       .attr("y", function(d,i){ return i * (size + 5) + (size/2)}) // 100 is where the first dot appears. 25 is the distance between dots
+       .style("fill", function(d){ return color(d)})
+       .text(function(d){ return d})
+       .attr("text-anchor", "left")
+       .style("alignment-baseline", "middle")
+       .on("mouseover", highlight)
+       .on("mouseleave", noHighlight)
+  // Features of the forces applied to the nodes:
+  var simulation = d3.forceSimulation()
+      .force("center", d3.forceCenter().x(width / 2).y(height / 2)) // Attraction to the center of the svg area
+      .force("charge", d3.forceManyBody().strength(.1)) // Nodes are attracted one each other of value is > 0
+      .force("collide", d3.forceCollide().strength(.2).radius(function(d){ return (z(d.value)+3) }).iterations(1)) // Force that avoids circle overlapping
 
-            return d.HomeTeam + ": " + d.Win;
-        });
+  // Apply these forces to the nodes and update their positions.
+  // Once the force algorithm is happy with positions ('alpha' value is low enough), simulations will stop.
+  simulation
+      .nodes(data)
+      .on("tick", function(d){
+        node
+            .attr("cx", function(d){ return d.x; })
+            .attr("cy", function(d){ return d.y; })
+      });
 
-    node.append("circle")
-        .attr("r", function(d) {
-            return d.r;
-        })
-        .style("fill", function(d,i) {
-            return color(i);
-        });
-    node.append("text")
-        .attr("dy", ".2em")
-        .style("text-anchor", "middle")
-        .text(function(d) {
-            console.log(d.data[0].HomeTeam)
-            return d.data[0].HomeTeam.substring(0, d.r / 3);
-        })
-        .attr("font-family", "sans-serif")
-        .attr("font-size", function(d){
-            return d.r/5;
-        })
-        .attr("fill", "white");
+  // What happens when a circle is dragged?
+  function dragstarted(d) {
+    if (!d3.event.active) simulation.alphaTarget(.03).restart();
+    d.fx = d.x;
+    d.fy = d.y;
+  }
+  function dragged(d) {
+    d.fx = d3.event.x;
+    d.fy = d3.event.y;
+  }
+  function dragended(d) {
+    if (!d3.event.active) simulation.alphaTarget(.03);
+    d.fx = null;
+    d.fy = null;
+  }
 
-    node.append("text")
-        .attr("dy", "1.3em")
-        .style("text-anchor", "middle")
-        .text(function(d) {
-            console.log(d.data[0].Win)
-            return d.data[0].Win;
-        })
-        .attr("font-family",  "Gill Sans", "Gill Sans MT")
-        .attr("font-size", function(d){
-            return d.r/5;
-        })
-        .attr("fill", "white");
-
-    d3.select(self.frameElement)
-        .style("height", diameter + "px");
-  })
+})
