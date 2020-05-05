44b7eacf0a267fe4681bc37c68b92a06ce6be039
Gerald Sula <gerald.sula@epfl.ch>
2020-05-05 17:11:39 +0200

    added first interactive accumulativeChart

--- skeleton/accumulating_chart/script_accumulating_interactive.js
+++ skeleton/accumulating_chart/script_accumulating_interactive.js
@@ -1,9 +1,9 @@
 
 
 // set the dimensions and margins of the graph
-var margin = {top: 10, right: 100, bottom: 30, left: 30},
-    width = 460 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
+var margin = {top: 10, right: 100, bottom: 500, left: 30},
+    width = 1000 - margin.left - margin.right,
+    height = 400 - margin.top ;
 
 // append the svg object to the body of the page
 var svg = d3.select("#my_dataviz")
@@ -15,10 +15,10 @@ var svg = d3.select("#my_dataviz")
           "translate(" + margin.left + "," + margin.top + ")");
 
 //Read the data
-d3.csv("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/data_connectedscatter.csv", function(data) {
+d3.csv("https://raw.githubusercontent.com/com-480-data-visualization/com-480-project-coviz-19/master/data/Computed/AccumulatingChart/merged.csv", function(data) {
 
     // List of groups (here I have one group per column)
-    var allGroup = ["valueA", "valueB", "valueC"]
+    var allGroup = ["ReturnSP1_2018_1_risky","ReturnSP1_2018_10_risky","ReturnSP1_2018_1","ReturnSP1_2018_10","ReturnI1_2018_1_risky","ReturnI1_2018_10_risky","ReturnI1_2018_1","ReturnI1_2018_10","ReturnF1_2018_1_risky","ReturnF1_2018_10_risky","ReturnF1_2018_1","ReturnF1_2018_10","ReturnD1_2018_1_risky","ReturnD1_2018_10_risky","ReturnD1_2018_1","ReturnD1_2018_10","ReturnE0_2018_1_risky","ReturnE0_2018_10_risky","ReturnE0_2018_1","ReturnE0_2018_10"]
 
     // add the options to the button
     d3.select("#selectButton")
@@ -36,7 +36,7 @@ d3.csv("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/da
 
     // Add X axis --> it is a date format
     var x = d3.scaleLinear()
-      .domain([0,10])
+      .domain([0,33])
       .range([ 0, width ]);
     svg.append("g")
       .attr("transform", "translate(0," + height + ")")
@@ -44,10 +44,20 @@ d3.csv("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/da
 
     // Add Y axis
     var y = d3.scaleLinear()
-      .domain( [0,20])
+      .domain( [-200,200])
       .range([ height, 0 ]);
     svg.append("g")
       .call(d3.axisLeft(y));
+
+
+
+var zero_line = svg.append("line")
+                     .attr("x1", 0)
+                     .attr("y1", y(0))
+                     .attr("x2", width)
+                     .attr("y2", y(0))
+                     .attr("stroke-width", 2)
+                     .attr("stroke", "red");
 
     // Initialize line with group a
     var line = svg
@@ -55,10 +65,10 @@ d3.csv("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/da
       .append("path")
         .datum(data)
         .attr("d", d3.line()
-          .x(function(d) { return x(+d.time) })
-          .y(function(d) { return y(+d.valueA) })
+          .x(function(d) { return x(+d.Week) })
+          .y(function(d) { return y(+d.ReturnSP1_2018_1_risky) })
         )
-        .attr("stroke", function(d){ return myColor("valueA") })
+        .attr("stroke", function(d){ return myColor("ReturnSP1_2018_1_risky") })
         .style("stroke-width", 4)
         .style("fill", "none")
 
@@ -66,7 +76,7 @@ d3.csv("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/da
     function update(selectedGroup) {
 
       // Create new data with the selection?
-      var dataFilter = data.map(function(d){return {time: d.time, value:d[selectedGroup]} })
+      var dataFilter = data.map(function(d){return {Week: d.Week, value:d[selectedGroup]} })
 
       // Give these new data to update line
       line
@@ -74,7 +84,7 @@ d3.csv("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/da
           .transition()
           .duration(1000)
           .attr("d", d3.line()
-            .x(function(d) { return x(+d.time) })
+            .x(function(d) { return x(+d.Week) })
             .y(function(d) { return y(+d.value) })
           )
           .attr("stroke", function(d){ return myColor(selectedGroup) })
