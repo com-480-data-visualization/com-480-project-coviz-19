b2c5c53c040dcef3312b3f24b1d1f2e429e886bb
Gerald Sula <gerald.sula@epfl.ch>
2020-05-17 17:25:24 +0200

    improved accumulating graph

--- skeleton/accumulating_chart/script_accumulating_interactive.js
+++ skeleton/accumulating_chart/script_accumulating_interactive.js
@@ -1,7 +1,7 @@
 
 
 // set the dimensions and margins of the graph
-var margin = {top: 10, right: 100, bottom: 500, left: 30},
+var margin = {top: 10, right: 100, bottom: 500, left: 50},
     width = 1000 - margin.left - margin.right,
     height = 400 - margin.top ;
 
@@ -14,20 +14,43 @@ var svg = d3.select("#my_dataviz")
     .attr("transform",
           "translate(" + margin.left + "," + margin.top + ")");
 
+    var selectedCountry="ReturnSP1_2018";
+    var selectedMoney="1";
+    var selectedRisky="";
+
 //Read the data
 d3.csv("https://raw.githubusercontent.com/com-480-data-visualization/com-480-project-coviz-19/master/data/Computed/AccumulatingChart/merged.csv", function(data) {
 
     // List of groups (here I have one group per column)
     var allGroup = ["ReturnSP1_2018_1_risky","ReturnSP1_2018_10_risky","ReturnSP1_2018_1","ReturnSP1_2018_10","ReturnI1_2018_1_risky","ReturnI1_2018_10_risky","ReturnI1_2018_1","ReturnI1_2018_10","ReturnF1_2018_1_risky","ReturnF1_2018_10_risky","ReturnF1_2018_1","ReturnF1_2018_10","ReturnD1_2018_1_risky","ReturnD1_2018_10_risky","ReturnD1_2018_1","ReturnD1_2018_10","ReturnE0_2018_1_risky","ReturnE0_2018_10_risky","ReturnE0_2018_1","ReturnE0_2018_10"]
 
+var optionsSelectButtonCountry=[["Italy 2018","ReturnI1_2018"],["Spain 2018","ReturnSP1_2018"],["France 2018","ReturnF1_2018"],["Spain 2018","ReturnE0_2018"],["Germany 2018", "ReturnD1_2018"]]
+var optionsSelectButtonMoney=[["1 CHF every match","1"],["10 CHF every match", "10"]]
+var optionsSelectButtonRisky=[["Follow bookies advice",""],["Do the opositie","risky"]]
     // add the options to the button
-    d3.select("#selectButton")
+    d3.select("#selectButtonCountry")
       .selectAll('myOptions')
-      .data(allGroup)
+      .data(optionsSelectButtonCountry)
       .enter()
       .append('option')
-      .text(function (d) { return d; }) // text showed in the menu
-      .attr("value", function (d) { return d; }) // corresponding value returned by the button
+      .text(function (d) { return d[0]; }) // text showed in the menu
+      .attr("value", function (d) { return d[1]; }) // corresponding value returned by the button
+
+          d3.select("#selectButtonMoney")
+      .selectAll('myOptions')
+      .data(optionsSelectButtonMoney)
+      .enter()
+      .append('option')
+      .text(function (d) { return d[0]; }) // text showed in the menu
+      .attr("value", function (d) { return d[1]; }) // corresponding value returned by the button
+
+          d3.select("#selectButtonRisky")
+      .selectAll('myOptions')
+      .data(optionsSelectButtonRisky)
+      .enter()
+      .append('option')
+      .text(function (d) { return d[0]; }) // text showed in the menu
+      .attr("value", function (d) { return d[1]; }) // corresponding value returned by the button
 
     // A color scale: one color for each group
     var myColor = d3.scaleOrdinal()
@@ -42,6 +65,14 @@ d3.csv("https://raw.githubusercontent.com/com-480-data-visualization/com-480-pro
       .attr("transform", "translate(0," + height + ")")
       .call(d3.axisBottom(x));
 
+        // text label for the x axis
+  svg.append("text")             
+      .attr("transform",
+            "translate(" + (width/2) + " ," + 
+                           (height + margin.top + 40) + ")")
+      .style("text-anchor", "middle")
+      .text("Week Number");
+
     // Add Y axis
     var y = d3.scaleLinear()
       .domain( [-200,200])
@@ -49,9 +80,16 @@ d3.csv("https://raw.githubusercontent.com/com-480-data-visualization/com-480-pro
     svg.append("g")
       .call(d3.axisLeft(y));
 
+        svg.append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 0 - margin.left)
+      .attr("x",0 - (height / 2))
+      .attr("dy", "1em")
+      .style("text-anchor", "middle")
+      .text("Return in CHF"); 
+
 
  var bisect = d3.bisector(function(d) { return d.Week; }).left;
- console.log(bisect)
 
   // Create the circle that travels along the curve of chart
   var focus = svg
@@ -130,7 +168,6 @@ var zero_line = svg.append("line")
 
     selectedData = data[i]
 
-console.log(selectedData.Week)
     focus
       .attr("cx", x(selectedData.Week))
       .attr("cy", y(selectedData[selectedGroup]))
@@ -147,14 +184,38 @@ console.log(selectedData.Week)
 
 
     //INITIALIZE
-    update("ReturnSP1_2018_1_risky")
+    update("ReturnI1_2018_1")
 
     // When the button is changed, run the updateChart function
-    d3.select("#selectButton").on("change", function(d) {
+    d3.select("#selectButtonCountry").on("change", function(d) {
         // recover the option that has been chosen
-        var selectedOption = d3.select(this).property("value")
+        selectedCountry = d3.select(this).property("value")
         // run the updateChart function with this selected option
-        update(selectedOption)
+        update(concatenate_options())
     })
+
+        d3.select("#selectButtonMoney").on("change", function(d) {
+        // recover the option that has been chosen
+        selectedMoney = d3.select(this).property("value")
+        console.log(concatenate_options())
+        // run the updateChart function with this selected option
+        update(concatenate_options())
+    })
+
+            d3.select("#selectButtonRisky").on("change", function(d) {
+        // recover the option that has been chosen
+        selectedRisky = d3.select(this).property("value")
+        // run the updateChart function with this selected option
+        update(concatenate_options())
+    })
+
+            function concatenate_options(){
+            	var res= selectedCountry+"_"+selectedMoney+"_"+selectedRisky;
+            	if(selectedRisky=="")
+            	{
+            		return selectedCountry+"_"+selectedMoney
+            	}
+            	else return res
+            }
 
 })
