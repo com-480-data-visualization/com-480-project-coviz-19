13d2927259a296a91539a6684f725344f10c238a
Georgios Fotiadis <georgios.fotiadis@epfl.ch>
2020-05-25 14:59:58 +0200

    made barchart more interactive

--- full_website/barchart_script.js
+++ full_website/barchart_script.js
@@ -1,98 +1,160 @@
- var dataIT="https://raw.githubusercontent.com/com-480-data-visualization/com-480-project-coviz-19/master/full_website/data/barchart/barchartI1.csv";
-  var dataFR="https://raw.githubusercontent.com/com-480-data-visualization/com-480-project-coviz-19/master/full_website/data/barchart/barchartF1.csv";
+var dataIT = "https://raw.githubusercontent.com/com-480-data-visualization/com-480-project-coviz-19/master/full_website/data/barchart/barchartI1.csv";
+var dataFR = "https://raw.githubusercontent.com/com-480-data-visualization/com-480-project-coviz-19/master/full_website/data/barchart/barchartF1.csv";
+
+var optionsSelectButtonProvider = [["Bet365", "b365"], ["BWIN", "bw"], ["Interwetten", "iw"], ["Pinnacle", "ps"], ["VC Bet", "vc"], ["William Hill", "wh"]]
+var optionsSelectButtonYear = [["2013", "2013"], ["2014", "2014"], ["2015", "2015"], ["2016", "2016"], ["2017", "2017"], ["2018", "2018"], ["2019", "2019"]]
+var optionsSelectButtonCountry = [["Italy", "I1"], ["Germany", "D1"], ["Spain", "SP1"], ["England", "E0"], ["France", "F1"]]
+
+d3.select("#barselectButtonProvider")
+  .selectAll('myOptions')
+  .data(optionsSelectButtonProvider)
+  .enter()
+  .append('option')
+  .text(function (d) { return d[0]; }) // text showed in the menu
+  .attr("value", function (d) { return d[1]; }) // corresponding value returned by the button
+
+d3.select("#barselectButtonYear")
+  .selectAll('myOptions')
+  .data(optionsSelectButtonYear)
+  .enter()
+  .append('option')
+  .text(function (d) { return d[0]; }) // text showed in the menu
+  .attr("value", function (d) { return d[1]; }) // corresponding value returned by the button
+
+d3.select("#barselectButtonLeague")
+  .selectAll('myOptions')
+  .data(optionsSelectButtonCountry)
+  .enter()
+  .append('option')
+  .text(function (d) { return d[0]; }) // text showed in the menu
+  .attr("value", function (d) { return d[1]; }) // corresponding value returned by the button
+  
+
 
 // set the dimensions and marginBarcharts of the graph
-var marginBarchart = {top: 10, right: 30, bottom: 20, left: 50},
-    widthBarchart = 1000 - marginBarchart.left - marginBarchart.right,
-    heightBarchart = 400 - marginBarchart.top - marginBarchart.bottom;
+var marginBarchart = { top: 10, right: 30, bottom: 20, left: 50 },
+  widthBarchart = 1200 - marginBarchart.left - marginBarchart.right,
+  heightBarchart = 400 - marginBarchart.top - marginBarchart.bottom;
 
 // append the svgBarchart object to the body of the page
 var svgBarchart = d3.select("#barchart")
   .append("svg")
-    .attr("width", widthBarchart + marginBarchart.left + marginBarchart.right)
-    .attr("height", heightBarchart + marginBarchart.top + marginBarchart.bottom)
+  .attr("width", widthBarchart + marginBarchart.left + marginBarchart.right)
+  .attr("height", heightBarchart + marginBarchart.top + marginBarchart.bottom)
   .append("g")
-    .attr("transform",
-          "translate(" + marginBarchart.left + "," + marginBarchart.top + ")");
+  .attr("transform", "translate(" + marginBarchart.left + "," + marginBarchart.top + ")");
 
-      // Add X axis
-  var xBarchart = d3.scaleBand()
-      // .domain(groups)
-      .range([0, widthBarchart])
-      .padding([0.2])
-  var xAxis = svgBarchart.append("g")
-    .attr("transform", "translate(0," + heightBarchart + ")")
-    .call(d3.axisBottom(xBarchart).tickSizeOuter(0));
+// Add X axis
+var xBarchart = d3.scaleBand()
+  // .domain(groups)
+  .range([0, widthBarchart])
+  .padding([0.2])
+var xAxis = svgBarchart.append("g")
+  .attr("transform", "translate(0," + heightBarchart + ")")
+  .call(d3.axisBottom(xBarchart).tickSizeOuter(0));
 
 
-  // Add Y axis
-  var yBarchart = d3.scaleLinear()
-    // .domain([0, 100])
-    .range([ heightBarchart, 0 ]);
- var yAxis =  svgBarchart.append("g")
-    .call(d3.axisLeft(yBarchart));
+// Add Y axis
+var yBarchart = d3.scaleLinear()
+  // .domain([0, 100])
+  .range([heightBarchart, 0]);
+var yAxis = svgBarchart.append("g")
+  .call(d3.axisLeft(yBarchart));
 
 function update_barchart(data_csv) {
-// Parse the Data
-d3.csv(data_csv, function(data) {
+  // Parse the Data
+  d3.csv(data_csv, function (data) {
 
-  // Update the X axis
-  xBarchart.domain(data.map(function(d) { return d.name; }))
-  xAxis.call(d3.axisBottom(xBarchart))
+    // Update the X axis
+    xBarchart.domain(data.map(function (d) { return d.name; }))
+    xAxis.call(d3.axisBottom(xBarchart))
 
-  // Update the Y axis
-  yBarchart.domain([0, 100])
-  yAxis.transition().duration(1000).call(d3.axisLeft(yBarchart));
+    // Update the Y axis
+    yBarchart.domain([0, 100])
+    yAxis.transition().duration(1000).call(d3.axisLeft(yBarchart));
 
-  // List of subgroups = header of the csv files = soil condition here
-  var subgroups = data.columns.slice(1)
-
-
-  // List of groups = species here = value of the first column called group -> I show them on the X axis
-  var groups = d3.map(data, function(d){return(d.name)}).keys()
+    // List of subgroups = header of the csv files = soil condition here
+    var subgroups = data.columns.slice(1)
 
 
+    // List of groups = species here = value of the first column called group -> I show them on the X axis
+    var groups = d3.map(data, function (d) { return (d.name) }).keys()
 
 
 
-  // color palette = one color per subgroup
-  var color = d3.scaleOrdinal()
-    .domain(subgroups)
-    .range(['#008000','#e41a1c'])
-
-  // Normalize the data -> sum of each group must be 100!
-  dataNormalized = []
-  data.forEach(function(d){
-    // Compute the total
-    tot = 0
-    for (i in subgroups){ name=subgroups[i] ; tot += +d[name] }
-    // Now normalize
-    for (i in subgroups){ name=subgroups[i] ; d[name] = d[name] / tot * 100}
-  })
 
 
-  //stack the data? --> stack per subgroup
-  var stackedData = d3.stack()
-    .keys(subgroups)
-    (data)
+    // color palette = one color per subgroup
+    var color = d3.scaleOrdinal()
+      .domain(subgroups)
+      .range(['#008000', '#e41a1c'])
+
+    // Normalize the data -> sum of each group must be 100!
+    dataNormalized = []
+    data.forEach(function (d) {
+      // Compute the total
+      tot = 0
+      for (i in subgroups) { name = subgroups[i]; tot += +d[name] }
+      // Now normalize
+      for (i in subgroups) { name = subgroups[i]; d[name] = d[name] / tot * 100 }
+    })
 
 
-  // Show the bars
-  svgBarchart.append("g")
-    .selectAll("g")
-    // Enter in the stack data = loop key per key = group per group
-    .data(stackedData)
-    .enter().append("g")
-      .attr("fill", function(d) { return color(d.key); })
+    //stack the data? --> stack per subgroup
+    var stackedData = d3.stack()
+      .keys(subgroups)
+      (data)
+
+
+    // Show the bars
+    svgBarchart.append("g")
+      .selectAll("g")
+      // Enter in the stack data = loop key per key = group per group
+      .data(stackedData)
+      .enter().append("g")
+      .attr("fill", function (d) { return color(d.key); })
       .selectAll("rect")
       // enter a second time = loop subgroup per subgroup to add all rectangles
-      .data(function(d) { 
-        return d; })
+      .data(function (d) {
+        return d;
+      })
       .enter().append("rect")
-        .attr("x", function(d) { return xBarchart(d.data.name); })
-        .attr("y", function(d) { return yBarchart(d[1]); })
-        .attr("height", function(d) { return yBarchart(d[0]) - yBarchart(d[1]); })
-        .attr("width",xBarchart.bandwidth())
-})
+      .attr("x", function (d) { return xBarchart(d.data.name); })
+      .attr("y", function (d) { return yBarchart(d[1]); })
+      .attr("height", function (d) { return yBarchart(d[0]) - yBarchart(d[1]); })
+      .attr("width", xBarchart.bandwidth())
+  })
 }
-update_barchart(dataIT)
+
+d3.select("#barselectButtonProvider").on("change", function (d) {
+  // recover the option that has been chosen
+  barselectedProvider = d3.select(this).property("value")
+  // run the updateChart function with this selected option
+  update_barchart(concatenate_options_bar())
+})
+
+d3.select("#barselectButtonYear").on("change", function (d) {
+  // recover the option that has been chosen
+  barselectedYear = d3.select(this).property("value")
+  // run the updateChart function with this selected option
+  update_barchart(concatenate_options_bar())
+})
+
+d3.select("#barselectButtonLeague").on("change", function (d) {
+  // recover the option that has been chosen
+  barselectedLeague = d3.select(this).property("value")
+  // run the updateChart function with this selected option
+  update_barchart(concatenate_options_bar())
+})
+
+function concatenate_options_bar() {
+
+  return "https://raw.githubusercontent.com/com-480-data-visualization/com-480-project-coviz-19/master/data/Computed/barchart/" + barselectedLeague + "_" + barselectedYear + "_" + barselectedProvider + ".csv"
+
+}
+
+barselectedProvider = "b365"
+barselectedYear = "2013"
+barselectedLeague = "I1"
+
+update_barchart(concatenate_options_bar())
--- full_website/map_script.js
+++ full_website/map_script.js
@@ -6,151 +6,151 @@ var height_map = 650
 // The svg
 var svg_map = d3.select("#map_viz")
   .append("svg")
- .attr("width", width_map)
+  .attr("width", width_map)
   .attr("height", height_map)
 
 
 var projection = d3.geoMercator()
   .scale(800)
-  .center([10,50])
+  .center([10, 50])
   .translate([width_map / 2, height_map / 2]);
 
 
 var Tooltip_fixed_map = d3.select("#map_info")
-.append("div")
-.style("opacity", 1)
-.attr("class", "tooltip")
-.style("background-color", "white")
-.style("border", "solid")
-.style("border-width", "2px")
-.style("border-radius", "5px")
-.style("padding", "5px")
-.attr("y", 0)
-.attr("x", 0)
-.style("width",'150px')
-.style("height",'50px')
-.html("Hover over a country")
+  .append("div")
+  .style("opacity", 1)
+  .attr("class", "tooltip")
+  .style("background-color", "white")
+  .style("border", "solid")
+  .style("border-width", "2px")
+  .style("border-radius", "5px")
+  .style("padding", "5px")
+  .attr("y", 0)
+  .attr("x", 0)
+  .style("width", '150px')
+  .style("height", '50px')
+  .html("Hover over a country")
 
 // Data and color scale
 var data_map = d3.map();
 var colorScale = d3.scaleLinear()
-  .domain([40,63])
+  .domain([40, 63])
   .range(["black", "green"]);
 
 
-var optionsSelectButtonProvider=[["Bet365","b365"],["BWIN","bw"],["IW", "iw"],["LB","lb"],["VC","vc"],["WH","wh"]]
-var optionsSelectButtonYear=[["2008/2009","2008_2009"],["2009/2010","2009_2010"],["2010/2011","2010_2011"],["2011/2012","2011_2012"],["2012/2013","2012_2013"],["2013/2014","2013_2014"],["2014/2015","2014_2015"],["2015/2016","2015_2016"]]
+var optionsSelectButtonProvider = [["Bet365", "b365"], ["BWIN", "bw"], ["Interwetten", "iw"], ["Ladbrokes", "lb"], ["VC Bet", "vc"], ["William Hill", "wh"]]
+var optionsSelectButtonYear = [["2008/2009", "2008_2009"], ["2009/2010", "2009_2010"], ["2010/2011", "2010_2011"], ["2011/2012", "2011_2012"], ["2012/2013", "2012_2013"], ["2013/2014", "2013_2014"], ["2014/2015", "2014_2015"], ["2015/2016", "2015_2016"]]
 
 d3.select("#selectButtonProvider")
-      .selectAll('myOptions')
-      .data(optionsSelectButtonProvider)
-      .enter()
-      .append('option')
-      .text(function (d) { return d[0]; }) // text showed in the menu
-      .attr("value", function (d) { return d[1]; }) // corresponding value returned by the button
+  .selectAll('myOptions')
+  .data(optionsSelectButtonProvider)
+  .enter()
+  .append('option')
+  .text(function (d) { return d[0]; }) // text showed in the menu
+  .attr("value", function (d) { return d[1]; }) // corresponding value returned by the button
 
 d3.select("#selectButtonYear")
-      .selectAll('myOptions')
-      .data(optionsSelectButtonYear)
-      .enter()
-      .append('option')
-      .text(function (d) { return d[0]; }) // text showed in the menu
-      .attr("value", function (d) { return d[1]; }) // corresponding value returned by the button
+  .selectAll('myOptions')
+  .data(optionsSelectButtonYear)
+  .enter()
+  .append('option')
+  .text(function (d) { return d[0]; }) // text showed in the menu
+  .attr("value", function (d) { return d[1]; }) // corresponding value returned by the button
 
 function update(data_csv) {
 
 
 
-    // Load external data and boot
-    d3.queue()
-      .defer(d3.json, "https://raw.githubusercontent.com/com-480-data-visualization/com-480-project-coviz-19/master/full_website/data/map/mymap.geojson")
-      .defer(d3.csv, data_csv, function(d) { data_map.set(d.country_name, parseFloat(d.accuracy_percentage).toFixed(2)); })
-      .await(ready);
+  // Load external data and boot
+  d3.queue()
+    .defer(d3.json, "https://raw.githubusercontent.com/com-480-data-visualization/com-480-project-coviz-19/master/full_website/data/map/mymap.geojson")
+    .defer(d3.csv, data_csv, function (d) { data_map.set(d.country_name, parseFloat(d.accuracy_percentage).toFixed(2)); })
+    .await(ready);
 
-    function ready(error, topo) {
+  function ready(error, topo) {
 
-      let mouseOver = function(d) {
-        d3.selectAll(".Country")
-          .transition()
-          .duration(200)
-          .style("opacity", .5)
-        d3.select(this)
-          .transition()
-          .duration(200)
-          .style("opacity", 1)
-          .style("stroke", "black")
-        Tooltip_fixed_map
+    let mouseOver = function (d) {
+      d3.selectAll(".Country")
+        .transition()
+        .duration(200)
+        .style("opacity", .5)
+      d3.select(this)
+        .transition()
+        .duration(200)
         .style("opacity", 1)
-    
-      }
-      let mousemove = function(d) {
+        .style("stroke", "black")
+      Tooltip_fixed_map
+        .style("opacity", 1)
 
-        Tooltip_fixed_map
+    }
+    let mousemove = function (d) {
+
+      Tooltip_fixed_map
         .html('<u>' + d.properties.name + '</u>' + "<br>" + d.total + "% accuracy")
-        .style("left", d3.mouse(this)[0]  + "px")
-        .style("top", d3.mouse(this)[1] -600+ "px")
-      }
-      let mouseLeave = function(d) {
-        d3.selectAll(".Country")
-          .transition()
-          .duration(200)
-          .style("opacity", .8)
-        d3.select(this)
-          .transition()
-          .duration(200)
-          .style("stroke", "transparent")
-          Tooltip_fixed_map
-          .html("Hover over a country")
-      }
+        .style("left", d3.mouse(this)[0] + "px")
+        .style("top", d3.mouse(this)[1] - 600 + "px")
+    }
+    let mouseLeave = function (d) {
+      d3.selectAll(".Country")
+        .transition()
+        .duration(200)
+        .style("opacity", .8)
+      d3.select(this)
+        .transition()
+        .duration(200)
+        .style("stroke", "transparent")
+      Tooltip_fixed_map
+        .html("Hover over a country")
+    }
 
-      // Draw the map
-      svg_map.append("g")
-        .selectAll("path")
-        .data(topo.features)
-        .enter()
-        .append("path")
-          // draw each country
-          .attr("d", d3.geoPath()
-            .projection(projection)
-          )
-          // set the color of each country
-          .attr("fill", function (d) {
-            d.total = data_map.get(d.id) || 0;
-            return colorScale(d.total);
-          })
-          .style("stroke", "transparent")
-          .attr("class", function(d){ return "Country" } )
-          .style("opacity", .8)
-          .on("mouseover", mouseOver )
-          .on("mousemove", mousemove)
-          .on("mouseleave", mouseLeave )
-        }
+    // Draw the map
+    svg_map.append("g")
+      .selectAll("path")
+      .data(topo.features)
+      .enter()
+      .append("path")
+      // draw each country
+      .attr("d", d3.geoPath()
+        .projection(projection)
+      )
+      // set the color of each country
+      .attr("fill", function (d) {
+        d.total = data_map.get(d.id) || 0;
+        return colorScale(d.total);
+      })
+      .style("stroke", "transparent")
+      .attr("class", function (d) { return "Country" })
+      .style("opacity", .8)
+      .on("mouseover", mouseOver)
+      .on("mousemove", mousemove)
+      .on("mouseleave", mouseLeave)
+  }
 }
 
 
-d3.select("#selectButtonProvider").on("change", function(d) {
-        // recover the option that has been chosen
-        selectedProvider = d3.select(this).property("value")
-        // run the updateChart function with this selected option
-        update(concatenate_options_map())
-      })
+d3.select("#selectButtonProvider").on("change", function (d) {
+  // recover the option that has been chosen
+  selectedProvider = d3.select(this).property("value")
+  // run the updateChart function with this selected option
+  update(concatenate_options_map())
+})
 
-d3.select("#selectButtonYear").on("change", function(d) {
-        // recover the option that has been chosen
-        selectedYear = d3.select(this).property("value")
-        // run the updateChart function with this selected option
-        update(concatenate_options_map())
-      })
+d3.select("#selectButtonYear").on("change", function (d) {
+  // recover the option that has been chosen
+  selectedYear = d3.select(this).property("value")
+  // run the updateChart function with this selected option
+  update(concatenate_options_map())
+})
 
-        //Initialize
+//Initialize
 
-        selectedProvider='b365'
-        selectedYear='2008_2009'
-
-
+selectedProvider = 'b365'
+selectedYear = '2008_2009'
 
 
-function concatenate_options_map(){
+
+
+function concatenate_options_map() {
 
   return "https://raw.githubusercontent.com/com-480-data-visualization/com-480-project-coviz-19/master/full_website/data/map/" + selectedProvider + "/" + selectedYear + ".csv"
 
